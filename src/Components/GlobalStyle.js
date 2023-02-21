8caeaa5fa9422362fb18e56f7869d5121fdae7f1
Felipe Arceno <arceno65@gmail.com>
2023-02-21 20:02:08 -0300

    Part 2: Tema Escuro e Claro

--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -7,8 +7,7 @@ export const GlobalStyle = createGlobalStyle`
   font-family: "Montserrat", sans-serif;
   margin: 0;
   padding: 0;
-  text-decoration: none;
-  color: grey;
+  text-decoration: none; 
 }
 
 `;
